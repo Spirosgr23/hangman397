899fb71d6f5b34693a5962f680005f0d3d2e8009
Spirosgr23 <63505432+Spirosgr23@users.noreply.github.com>
2023-11-26 19:52:52 +0000

    Add files via upload

---
+++ milestone_5.py
@@ -0,0 +1,51 @@
+import random
+
+class Hangman:
+    def __init__(self, word_list, num_lives=5):
+        self.word = random.choice(word_list).lower()
+        self.word_guessed = ['_' for _ in self.word]
+        self.num_letters = len(set(self.word))
+        self.num_lives = num_lives
+        self.word_list = word_list
+        self.list_of_guesses = []
+
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+            for i, letter in enumerate(self.word):
+                if letter == guess:
+                    self.word_guessed[i] = guess
+                    self.num_letters -= 1
+        else:
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word. You have {self.num_lives} lives left.")
+
+    def ask_for_input(self):
+        guess = input("Guess a letter: ").lower()
+
+        if not guess.isalpha() or len(guess) != 1:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+        elif guess in self.list_of_guesses:
+            print("You already tried that letter!")
+        else:
+            self.list_of_guesses.append(guess)
+            self.check_guess(guess)
+
+def play_game(word_list):
+    game = Hangman(word_list, num_lives=5)
+
+    while True:
+        if game.num_lives > 0 and game.num_letters > 0:
+            print(f"Word to guess: {' '.join(game.word_guessed)}")
+            game.ask_for_input()
+        elif game.num_lives == 0:
+            print('You lost!')
+            break
+        else:
+            print('Congratulations. You won the game!')
+            break
+
+if __name__ == "__main__":
+    words = ["Apple", "Banana", "Cherry", "Date", "Elderberry"]
+    play_game(words)
